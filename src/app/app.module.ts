fcda75341ecdca76bcadbcc676181c13c82951a9
Erivan <erivanfrank@hotmail.com>
2017-06-25 23:57:14 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, trigger, transition, style, state, animate, keyframes} from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MainPage} from'../pages/main/main';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MainPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserAnimationsModule 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MainPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, trigger, transition, style, state, animate, keyframes } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
+import { MainPage} from'../main/main';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	@ViewChild(Slides) Slides: Slides;
+	skipMsg: string = 'Skip';
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController) {
 
-  }
+	}
 
+	skip(){
+		this.navCtrl.push(MainPage);
+	}
+
+	slideChanged(){
+		if(this.Slides.isEnd())  
+			this.skipMsg = "Alright, I got it";
+	}
 }
